ec578e99a7308ae9d66bdcd4cb3f86c712502009
monster anshu <hgunwant2312@gmail.com>
2022-07-27 16:48:51 +0530

    DB

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import cors from 'cors';
 import morgan from 'morgan';
 import { router as posts_router } from './routes/posts.routes';
 import { router as comment_router } from './routes/comments.routes';
-
 import error_handler from './handler/error_handler';
 const PORT = process.env.PORT || 5000;
 
@@ -22,4 +21,5 @@ app.use(error_handler);
 
 const server = app.listen(PORT, async () => {
   console.log('Server is running');
+  const prisma = new PrismaClient();
 });
